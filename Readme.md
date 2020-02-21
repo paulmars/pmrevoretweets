81a651533b7739e7c815b31a5103991682a72d75
Paul McKellar <git@paulmckellar.com>
2020-02-21 12:59:42 -0800

    new babel stuff

--- Readme.md
+++ Readme.md
@@ -4,8 +4,8 @@ ITS DONE
 yarn install
 
 # compile
-npm install -g webpack
-webpack --mode=development
+npm install -g webpack webpack-cli
+webpack --mode=development -w
 
 # serve
 revore tweets-pm
--- src/app.jsx
+++ src/app.jsx
@@ -5,3 +5,20 @@ import ReactDOM from 'react-dom';
 fetch("/src/words/a.json")
     .then(response => response.json())
     .then(json => console.log(json));
+
+class Tweet extends React.Component {
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  React.createElement(Tweet, {}, null),
+  document.getElementById('app')
+);
+
+document.querySelectorAll('title')[0].textContent = "IMDB Top 250";
