88e6f5b8d903facb8b6e46c68461704e801402dc
Paul McKellar <git@paulmckellar.com>
2020-02-21 16:07:38 -0800

    see

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import superagent from 'superagent';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import lodash from 'lodash';
 
 class Tweet extends React.Component {
   constructor(props) {
@@ -45,14 +45,16 @@ class Tweet extends React.Component {
   }
 }
 
-class TweetList extends React.Component {
+class InputText extends React.Component {
   render() {
-    const tweets = this.props.tweetids.map(id => <Tweet key={id} tweetid={id} />)
+    const { change } = this.props
     return (
-      <div>
-        {tweets}
-      </div>
-    )
+      <input
+        type="text"
+        name="txt"
+        onChange={change}
+      />
+    );
   }
 }
 
@@ -63,10 +65,36 @@ function render(json) {
   );
 }
 
-fetch("/src/words/reddit.json")
-  .then(response => response.json())
-  .then(json => {
-    render(json);
-  });
+class TweetList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.getData = lodash.throttle(this.getData.bind(this), 0, {leading: true, trailing: true});
+  }
+
+  getData(value) {
+    const url = `/src/words/${value}.json`;
+    console.log(url);
+    fetch(url).then(response => response.json()).then(json => {
+      render(json);
+    });
+  }
+
+  handleChange(e) {
+    this.getData(e.target.value);
+  }
+
+  render() {
+    const tweets = this.props.tweetids.map(id => <Tweet key={id} tweetid={id} />)
+    return (
+      <div>
+        <InputText change={this.handleChange} />
+        {tweets}
+      </div>
+    );
+  }
+}
+
+render([])
 
 document.querySelectorAll('title')[0].textContent = "Twitter";
