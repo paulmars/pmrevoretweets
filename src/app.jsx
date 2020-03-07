c6d85aa81c676b1bfcab6e5d2c812556cd965806
Paul McKellar <git@paulmckellar.com>
2020-03-06 19:18:49 -0800

    days

--- src/app.jsx
+++ src/app.jsx
@@ -108,14 +108,59 @@ class InputText extends React.Component {
   }
 }
 
+class Year extends React.Component {
+  render() {
+    const { year } = this.props;
+    return (
+      <div className="year">
+        <button onClick={this.props.handleYear}>
+          {year}
+        </button>
+      </div>
+    )
+  }
+}
+
+class Month extends React.Component {
+  render() {
+    const { month } = this.props;
+    return (
+      <div className="month">
+        <button onClick={this.props.handleMonth}>
+          {month}
+        </button>
+      </div>
+    )
+  }
+}
+
+class Day extends React.Component {
+  render() {
+    const { day } = this.props;
+    return (
+      <div className="day">
+        <button onClick={this.props.handleDay}>
+          {day}
+        </button>
+      </div>
+    )
+  }
+}
+
 class TweetList extends React.Component {
   constructor(props) {
     super(props);
+    const d = new Date();
+    const years = lodash.range(d.getFullYear(), 2006);
     this.state = {
       tweetids: [],
+      years,
     }
     this.handleChange = this.handleChange.bind(this);
     this.getData = lodash.throttle(this.getData.bind(this), 1000, {leading: false, trailing: true});
+    this.handleYear = this.handleYear.bind(this);
+    this.handleMonth = this.handleMonth.bind(this);
+    this.handleDay = this.handleDay.bind(this);
   }
 
   componentDidMount() {
@@ -142,19 +187,40 @@ class TweetList extends React.Component {
     this.getData(v);
   }
 
+  handleYear(year) {
+    this.setState({ year })
+  }
+
+  handleMonth(month) {
+    this.setState({ month })
+  }
+
+  handleDay(day) {
+    this.setState({ day })
+  }
+
   render() {
     const tweets = this.state.tweetids.map(id => <Tweet key={id} tweetid={id} />)
+    const months = lodash.range(1, 12 + 1);
+    const daysInMonth = new Date(2012, 2, 0).getDate();
+    const days = lodash.range(1, daysInMonth + 1);
+
+    console.log(this.state)
+
     return (
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-sm-1">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-1">
             <h1>Years</h1>
+            {this.state.years.map(y => <Year key={`year${y}`} year={y} handleYear={() => this.handleYear(y)} />)}
           </div>
-          <div class="col-sm-1">
+          <div className="col-sm-1">
             <h1>Months</h1>
+            {months.map(m => <Month key={`month${m}`} month={m} handleMonth={() => this.handleMonth(m)} />)}
           </div>
-          <div class="col-sm-10">
-            <h1>Tweets</h1>
+          <div className="col-sm-10">
+            <h1>Days</h1>
+            {days.map(d => <Day key={`day${d}`} day={d} handleDay={() => this.handleDay(d)} />)}
           </div>
         </div>
       </div>
